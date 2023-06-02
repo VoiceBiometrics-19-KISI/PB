d69f6a46e870b78fdab9927add2025da28403c20
Aleksandra <alexrumi2000@gmail.com>
2023-06-02 02:01:30 +0200

    Add training recipe.

---
+++ training/run_prepare_cn.py
@@ -0,0 +1,9 @@
+from prepare_cn_celeb import prepare_cn_celeb
+import os
+# main path to CN Celeb data
+current_path = os.getcwd()
+parent_path = os.path.dirname(current_path)
+data_folder = os.path.join(parent_path, "CN_Celeb")
+# loads data from folders and divides them to three sets (train val test) in 80:10:10 ratio
+# data is expected with .wav extension <- if different to be provided <- change line 58 (prepare_cn_celeb.py)
+prepare_cn_celeb(data_folder, "train.json", "valid.json", "test.json")
